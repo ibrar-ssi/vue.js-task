f6fb5ee3b2f18939753575c53d261d7b97cba4e4
ibrar-ssi <mibrar@ssidecisions.com>
2024-04-03 12:26:22 +0500

    adding dist

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,7 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
+  publicPath: process.env.NODE_ENV === 'production'
+  ? '/vue.js-task/'
+  : '/',
   transpileDependencies: true
 })
